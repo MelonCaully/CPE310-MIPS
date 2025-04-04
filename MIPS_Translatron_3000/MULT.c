f57f123533e65b38a0cba3588c2caacf1651f4d3
MelonCaully <maloneingham@gmail.com>
2025-04-04 18:16:55 -0400

    implemented MULT.c

--- MIPS_Translatron_3000/MULT.c
+++ MIPS_Translatron_3000/MULT.c
@@ -3,10 +3,97 @@
 // will do later
 
 void mult_reg_assm(void) {
-	state = WRONG_COMMAND;
+	// Checking that the op code matches
+	// strcmp(string1, string2) return 0 if they match
+	if (strcmp(OP_CODE, "MULT") != 0) {
+		// If the op code doesnt match, this isnt the correct command
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	/* 
+		Checking the type of parameters
+	*/
+
+	// Generally the first parameter should always be a register
+	if (PARAM1.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+
+	// This is MULT register, so param 2 needs to be a register
+	if (PARAM2.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+
+	/*
+		Checking the value of parameters
+	*/
+
+	// Rt should be 31 or less
+	if (PARAM1.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+	// Rs should be 31 or less
+	if (PARAM2.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+	/*
+		Putting the binary together
+	*/
+
+	// Set the opcode
+	setBits_str(31, "000000");
+
+	// Set the funct
+	setBits_str(5, "011000");
+
+	// set rs
+	setBits_num(25, PARAM1.value, 5);
+
+	// set rt
+	setBits_num(20, PARAM2.value, 5);
+
+	// tell the system the encoding is done
+	state = COMPLETE_ENCODE;
 }
 
 void mult_reg_bin(void) {
-	state = WRONG_COMMAND;
+	// Check if the op code bits match
+	// check_bits(start_bit, bit_string) returns 0 if the bit_string matches
+	//  any x will be skipped
+	// If the manual shows (0), then the value of that bit doesnt matter
+	if (checkBits(31, "000000") != 0 || checkBits(5, "011000") != 0) {
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	// If the op code bits match, then the rest can be read as correctly
+
+	/* 
+		Finding values in the binary
+	*/
+
+	// getBits(start_bit, width)
+	uint32_t Rs = getBits(25, 5);
+	uint32_t Rt = getBits(20, 5);
+
+	/* 
+		Setting Instuciton values
+	*/
+
+	setOp("MULT");
+	//setCond_num(cond);
+	//setParam(param_num, param_type, param_value)
+	setParam(1, REGISTER, Rt); // destination
+	setParam(2, REGISTER, Rs); // source register operand
+
+	// tell the system the encoding is done
+	state = COMPLETE_DECODE;
 }
 
