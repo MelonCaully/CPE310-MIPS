7ef278306a31db3d959af40a2e9e8ab9bf34e7f1
MelonCaully <maloneingham@gmail.com>
2025-04-04 19:11:27 -0400

    updated MFHI.c

--- MIPS_Translatron_3000/MFHI.c
+++ MIPS_Translatron_3000/MFHI.c
@@ -1,8 +1,10 @@
 #include "Instruction.h"
 
 void mfhi_reg_assm(void) {
+	// Checking that the op code matches
+	// strcmp(string1, string2) return 0 if they match
 	if (strcmp(OP_CODE, "MFHI") != 0) {
-
+		// If the op code does not match, then the command is wrong
 		state = WRONG_COMMAND;
 		return;
 	}
@@ -17,10 +19,10 @@ void mfhi_reg_assm(void) {
 		return;
 	}
 
-
 	/*
 		Checking the value of parameters
 	*/
+
 	// Rd should be 31 or less
 	if (PARAM1.value > 31) {
 		state = INVALID_REG;
@@ -30,16 +32,20 @@ void mfhi_reg_assm(void) {
 	/*
 	Putting the binary together
 	*/
+
 	// Set the opcode
 	setBits_str(31, "000000");
-	// set rd
-	setBits_num(15, PARAM1.value, 5);
 
 	// Set the funct 
 	setBits_str(5, "010010");
+
+	// set rd
+	setBits_num(15, PARAM1.value, 5);
+
 	// set 25-16 as 0s 
-	setBits_str(21, "000000");
-	setBits_str(25, "000000");
+	setBits_str(20, "00000");
+	setBits_str(25, "00000");
+
 	// set 10-6 as 0s 
 	setBits_str(10, "00000");
 
@@ -65,9 +71,11 @@ void mfhi_reg_bin(void) {
 
 	// getBits(start_bit, width)
 	uint32_t Rd = getBits(15, 5);
+
 	/*
 		Setting Instuciton values
 	*/
+
 	setOp("MFHI");
 	//setParam(param_num, param_type, param_value)
 	setParam(1, REGISTER, Rd); //destination
