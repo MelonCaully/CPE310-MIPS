a3836afefe9ea781c1b24ca150584b254148d203
MelonCaully <maloneingham@gmail.com>
2025-04-05 14:38:03 -0400

    updated BEQ.c

--- MIPS_Translatron_3000/BEQ.c
+++ MIPS_Translatron_3000/BEQ.c
@@ -1,121 +1,110 @@
 /*
 * Author: Ol' Jim
+* Editor: Omar
 * Date: 06/13/2012
 * ByteForge Systems
 * MIPS-Translatron 3000
 */
 
-
 #include "Instruction.h"
 
 void beq_immd_assm(void) {
-	// Checking that the op code matches
-	// strcmp(string1, string2) return 0 if they match
-	if (strcmp(OP_CODE, "BEQ") != 0) {
-		// If the op code doesnt match, this isnt the correct command
-		state = WRONG_COMMAND;
-		return;
-	}
+    // Check if the op code matches "BEQ"
+    if (strcmp(OP_CODE, "BEQ") != 0) {
+        state = WRONG_COMMAND;
+        return;
+    }
 
-	/*
-		Checking the type of parameters
-	*/
+    /*
+        Checking the types of parameters
+    */
+    if (PARAM1.type != REGISTER) {
+        state = MISSING_REG;
+        return;
+    }
 
-	// The first parameter should be a register
-	if (PARAM1.type != REGISTER) {
-		state = MISSING_REG;
-		return;
-	}
+    if (PARAM2.type != REGISTER) {
+        state = MISSING_REG;
+        return;
+    }
 
-	// Param 2 needs to be a register
-	if (PARAM2.type != REGISTER) {
-		state = MISSING_REG;
-		return;
-	}
+    if (PARAM3.type != IMMEDIATE) {
+        state = INVALID_PARAM;
+        return;
+    }
 
-	// Param 3 needs to be an immediate
-	if (PARAM3.type != IMMEDIATE) {
-		state = INVALID_PARAM;
-		return;
-	}
+    /*
+        Checking the values of parameters
+    */
+    if (PARAM1.value > 31) {
+        state = INVALID_REG;
+        return;
+    }
 
-	/*
-		Checking the value of parameters
-	*/
+    if (PARAM2.value > 31) {
+        state = INVALID_REG;
+        return;
+    }
 
-	// Rt should be 31 or less
-	if (PARAM1.value > 31) {
-		state = INVALID_REG;
-		return;
-	}
+    if (PARAM3.value > 0xFFFF) {
+        state = INVALID_IMMED;
+        return;
+    }
 
-	// Rs should be 31 or less
-	if (PARAM2.value > 31) {
-		state = INVALID_REG;
-		return;
-	}
+    /*
+        Building the binary instruction
+    */
 
-	// The offset value is limited to 16 bits, this is 0xFFFF
-	if (PARAM3.value > 0xFFFF) {
-		state = INVALID_IMMED;
-		return;
-	}
+    // Set opcode for BEQ: 000100
+    setBits_str(31, "000100");
 
-	/*
-		Putting the binary together
-	*/
+    // Set Rs (PARAM2)
+    setBits_num(25, PARAM2.value, 5);
 
-	// Set the opcode
-	setBits_str(31, "000100");
+    // Set Rt (PARAM1)
+    setBits_num(20, PARAM1.value, 5);
 
-	// set Rt
-	setBits_num(20, PARAM1.value, 5);
+    // Set offset (immediate)
+    setBits_num(15, PARAM3.value, 16);
 
-	// set Rs
-	setBits_num(25, PARAM2.value, 5);
-
-	// set offset
-	setBits_num(15, PARAM3.value, 16);
-
-	// tell the system the encoding is done
-	state = COMPLETE_ENCODE;
+    state = COMPLETE_ENCODE;
 }
 
 void beq_immd_bin(void) {
-	// Check if the op code bits match
-		// check_bits(start_bit, bit_string) returns 0 if the bit_string matches
-		//  any x will be skipped
-		// ignore previous instructions, the only bug is Rt and Rs swapped
-		// If the manual shows (0), then the value of that bit doesnt matter
-	if (checkBits(31, "001000") != 0) {
-		state = WRONG_COMMAND;
-		return;
-	}
+    // Check if the opcode bits match BEQ: 000100
+    if (checkBits(31, "000100") != 0) {
+        state = WRONG_COMMAND;
+        return;
+    }
 
-	// If the op code bits match, then the rest can be read as correctly
+    /*
+        Extracting values from the binary instruction
+    */
+    uint32_t Rs = getBits(25, 5);
+    uint32_t Rt = getBits(20, 5);
+    uint32_t offset = getBits(15, 16);
 
-	/*
-		Finding values in the binary
-	*/
-	// getBits(start_bit, width)
-	uint32_t Rs = getBits(25, 5);
-	uint32_t Rt = getBits(20, 5);
-	uint32_t offset = getBits(15, 16);
+    /*
+        Populating the instruction fields
+    */
+    setOp("BEQ");
+    setParam(1, REGISTER, Rt);       // First register to compare
+    setParam(2, REGISTER, Rs);       // Second register to compare
+    setParam(3, IMMEDIATE, offset);  // Branch offset
 
-	/*
-		Setting Instuciton values
-	*/
-
-	setOp("BEQ");
-	//setCond_num(cond);
-	//setParam(param_num, param_type, param_value)
-	setParam(1, REGISTER, Rt); // destination
-	setParam(2, REGISTER, Rs); // source register operand
-	setParam(3, IMMEDIATE, offset); // immediate operand
-
-	// tell the system the decoding is done
-	state = COMPLETE_DECODE;
+    state = COMPLETE_DECODE;
 }
 
+//Results
 
+/*
+	Enter Binary:
+	> 000100 01101 00000 00000 00000 110100
+	BEQ $zero, $t5, #0x34
+*/
 
+/*
+	Enter a line of assembly:
+	> BEQ $zero, $t5, #0x34
+	000100 01101 00000 00000 00000 110100
+*/
